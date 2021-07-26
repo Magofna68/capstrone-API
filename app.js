33ae8f20f7ef50a0ae51f2e3a7df0bdefb401d7a
Brandon-Magofna <magofna68@gmail.com>
2021-07-26 16:43:18 -0700

    :hammer: WIP: API not functioning correctly. sending a 401 for all emails except my own.

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const sendGrid = require('@sendGrid/mail');
 
 const app = express();
-const API_KEY = process.env.REACT_APP_SENDGRID_API_KEY;
+// const API_KEY = process.env.REACT_APP_SENDGRID_API_KEY;
 
 app.use(bodyParser.json());
 
@@ -12,7 +12,7 @@ app.use(cors());
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'Get, Post, Put, Patch, Delete');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
@@ -22,31 +22,35 @@ app.get('/api', (req, res, next) => {
 });
 
 app.post('/api/email', (req, res, next) => {
-  sendGrid.setApiKey({ SENDGRID_API_KEY });
-})
 
+  console.log(req.body);
+  // sendGrid.setApiKey('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');
+  const SEND_GRID_API_KEY = process.env.REACT_APP_SENDGRID_API_KEY
 
-// const sgMail = require('@sendgrid/mail')
-// sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-const msg = {
-  to: 'Magofna68@gmail.com',
-  from: req.body.email,
-  subject: 'New Client: Web Contact',
-  text: req.body.message,
-  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-}
-sendGrid.send(msg);
+  // sendGrid.setApiKey({ SEND_GRID_API_KEY });
+  sendGrid.setApiKey('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');
+
+  const msg = {
+    to: 'Magofna68@gmail.com',
+    from: req.body.email,
+    subject: 'New Client: Web Contact',
+    text: req.body.message,
+    // html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  }
+
+  sendGrid.send(msg)
     .then(result => {
 
-  res.status(200).json({
-    success: true
-  })
-})
-  .catch(err => {
-    console.log('error: ', err);
-    res.status(401).json({
-      success: false
+      res.status(200).json({
+        success: true
+      })
     })
-  })
+    .catch(err => {
+      console.log('error: ', err);
+      res.status(401).json({
+        success: false
+      });
+    });
+});
 
 app.listen(3000, '0.0.0.0');
